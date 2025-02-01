78c2b2a8645771f502a6c9b49851f21199c36b68
HuaZhenting <142099827+HuaZhenting@users.noreply.github.com>
2025-02-02 03:31:53 +0800

    Rename, Add greet and exit

---
+++ src/main/java/Huke.java
@@ -0,0 +1,17 @@
+public class Huke {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _  | _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "| | | |\\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________\n" +
+                " Hello! I'm Huke\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n");
+        System.exit(0);
+    }
+}
