99fa04f9c5ee3cda6cc24350519125dc0fd18625
HuaZhenting <142099827+HuaZhenting@users.noreply.github.com>
2025-02-02 04:23:17 +0800

    Add echo

--- src/main/java/Huke.java
+++ src/main/java/Huke.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public class Huke {
+
+    public static void echo(String inputCommand) {
+        System.out.println(inputCommand);
+    }
+
+    public static void exit() {
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.exit(0);
+    }
     public static void main(String[] args) {
         System.out.println(" Hello! I'm Huke\n" + " What can I do for you?");
-        System.out.println(" Bye. Hope to see you again soon!\n");
-        System.exit(0);
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            String command = in.nextLine();
+            if (command.equals("bye")) {
+                exit();
+            } else {
+                echo(command);
+            }
+        }
     }
 }
