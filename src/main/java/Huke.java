526c60bcd4c0ba6cecac1ec7617c2ddb554c9cb8
HuaZhenting <142099827+HuaZhenting@users.noreply.github.com>
2025-02-02 17:53:27 +0800

    Add the list and list insertion

--- src/main/java/Huke.java
+++ src/main/java/Huke.java
@@ -1,10 +1,7 @@
 import java.util.Scanner;
 
 public class Huke {
-
-    public static void echo(String inputCommand) {
-        System.out.println(inputCommand);
-    }
+    
 
     public static void exit() {
         System.out.println(" Bye. Hope to see you again soon!");
@@ -13,13 +10,30 @@ public class Huke {
     public static void main(String[] args) {
         System.out.println(" Hello! I'm Huke\n" + " What can I do for you?");
         Scanner in = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
         while (true) {
             String command = in.nextLine();
             if (command.equals("bye")) {
                 exit();
+            } else if (command.equals("list")){
+                printTasks(taskCount, tasks);
             } else {
-                echo(command);
+                taskCount = addTask(tasks, taskCount, command);
             }
+        }
+    }
+
+    private static int addTask(String[] tasks, int taskCount, String command) {
+        tasks[taskCount] = command;
+        taskCount += 1;
+        System.out.println("added: " + command);
+        return taskCount;
+    }
+
+    private static void printTasks(int taskCount, String[] tasks) {
+        for (int i = 0; i < taskCount; i ++) {
+            System.out.println((i + 1) + ". " + tasks[i]);
         }
     }
 }
