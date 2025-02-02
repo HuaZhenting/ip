94d0d801d45320446ffe2a826fb18135be17b6b2
HuaZhenting <142099827+HuaZhenting@users.noreply.github.com>
2025-02-02 22:10:15 +0800

    Improve handling of incorrect user input

--- src/main/java/Huke.java
+++ src/main/java/Huke.java
@@ -18,26 +18,38 @@ public class Huke {
             } else if (command.equals("list")){
                 printTasks(taskCount, tasks);
             } else if (command.startsWith("mark")){
-                int position = Integer.parseInt(command.substring(5)) - 1;
-                if (position >= taskCount) {
-                    System.out.println("Task does not exist");
-                } else {
-                    tasks[position].setDone();
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(tasks[position]);
-                }
+                markTask(command, taskCount, tasks);
             } else if (command.startsWith("unmark")){
-                int position = Integer.parseInt(command.substring(7)) - 1;
-                if (position >= taskCount) {
-                    System.out.println("Task does not exist");
-                } else {
-                    tasks[position].setNotDone();
-                    System.out.println("OK, I've marked this task as not done yet:");
-                    System.out.println(tasks[position]);
-                }
+                unmarkTask(command, taskCount, tasks);
             } else {
                 taskCount = addTask(tasks, taskCount, command);
             }
+        }
+    }
+
+    private static void unmarkTask(String command, int taskCount, Task[] tasks) {
+        int position = Integer.parseInt(command.substring(7)) - 1;
+        if (position >= taskCount) {
+            System.out.println("Task does not exist");
+        } else if (tasks[position].getStatusIcon().equals(" ")) {
+            System.out.println("Task already marked not done");
+        } else {
+            tasks[position].setNotDone();
+            System.out.println("OK, I've marked this task as not done yet:");
+            System.out.println(tasks[position]);
+        }
+    }
+
+    private static void markTask(String command, int taskCount, Task[] tasks) {
+        int position = Integer.parseInt(command.substring(5)) - 1;
+        if (position >= taskCount) {
+            System.out.println("Task does not exist");
+        } else if (tasks[position].getStatusIcon().equals("X")) {
+            System.out.println("Task already marked done");
+        } else {
+            tasks[position].setDone();
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println(tasks[position]);
         }
     }
 
