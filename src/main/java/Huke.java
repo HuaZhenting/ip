634443fd9b4c203ec097675b2f2c567c4ad7221a
HuaZhenting <142099827+HuaZhenting@users.noreply.github.com>
2025-02-02 22:03:08 +0800

    Use a class to represent Task

--- src/main/java/Huke.java
+++ src/main/java/Huke.java
@@ -9,7 +9,7 @@ public class Huke {
     public static void main(String[] args) {
         System.out.println(" Hello! I'm Huke\n" + " What can I do for you?");
         Scanner in = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
         while (true) {
             String command = in.nextLine();
@@ -17,22 +17,40 @@ public class Huke {
                 exit();
             } else if (command.equals("list")){
                 printTasks(taskCount, tasks);
+            } else if (command.startsWith("mark")){
+                int position = Integer.parseInt(command.substring(5)) - 1;
+                if (position >= taskCount) {
+                    System.out.println("Task does not exist");
+                } else {
+                    tasks[position].setDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(tasks[position]);
+                }
+            } else if (command.startsWith("unmark")){
+                int position = Integer.parseInt(command.substring(7)) - 1;
+                if (position >= taskCount) {
+                    System.out.println("Task does not exist");
+                } else {
+                    tasks[position].setNotDone();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println(tasks[position]);
+                }
             } else {
                 taskCount = addTask(tasks, taskCount, command);
             }
         }
     }
 
-    private static int addTask(String[] tasks, int taskCount, String command) {
-        tasks[taskCount] = command;
+    private static int addTask(Task[] tasks, int taskCount, String command) {
+        tasks[taskCount] = new Task(command);
         taskCount += 1;
         System.out.println("added: " + command);
         return taskCount;
     }
 
-    private static void printTasks(int taskCount, String[] tasks) {
+    private static void printTasks(int taskCount, Task[] tasks) {
         for (int i = 0; i < taskCount; i ++) {
-            System.out.println((i + 1) + ". " + tasks[i]);
+            System.out.println((i + 1) + "." + tasks[i]);
         }
     }
 }
