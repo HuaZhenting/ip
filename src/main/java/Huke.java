cf6b4bac51001939228cfc86a806826430571c62
HuaZhenting <142099827+HuaZhenting@users.noreply.github.com>
2025-02-02 18:00:14 +0800

    Modify Structure

--- src/main/java/Huke.java
+++ src/main/java/Huke.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 
 public class Huke {
-    
-
     public static void exit() {
         System.out.println(" Bye. Hope to see you again soon!");
         System.exit(0);
     }
+    
     public static void main(String[] args) {
         System.out.println(" Hello! I'm Huke\n" + " What can I do for you?");
         Scanner in = new Scanner(System.in);
